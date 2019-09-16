d2aa96778389c53969d5c3fbb25f6880aaf6be47
kde0820 <kde0820@naver.com>
2019-09-16 15:58:31 +0900

    124나라의숫자-김다은

---
+++ 124나라의숫자/김다은/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void main(String[] args) {
+        int[] inputs = {1, 2, 3, 4};
+        for (int n : inputs) {
+            System.out.println(solution(n));
+        }
+    }
+
+    public static String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        String numbers = "124";
+        while(n > 0){
+            n--;
+            sb.insert(0, numbers.charAt(n%3));
+            n /= 3;
+        }
+        return sb.toString();
+    }
+}
